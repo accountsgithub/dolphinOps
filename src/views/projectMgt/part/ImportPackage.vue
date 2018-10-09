183efccadabb08bb33090f8cf3b745e15dbf88f1
xubaoshi <xubaoshi@kedacom.com>
2018-10-09 14:06:13 +0800

    change upload package size from 200M to 300M

--- src/views/projectMgt/part/ImportPackage.vue
+++ src/views/projectMgt/part/ImportPackage.vue
@@ -23,7 +23,7 @@
             <div class="el-upload__text" v-if="upLoadStatus === '0'">将文件拖到此处，或<em>点击上传</em></div>
             <div class="el-upload__text" v-else-if="upLoadStatus === '1'">文件上传中，请勿重复上传</div>
             <div class="el-upload__text" v-else-if="upLoadStatus === '2'"> <span style="color:red;">文件上传失败，请删除后重新上传</span></div>
-            <div class="el-upload__tip" slot="tip">只能上传rar/zip文件，且不超过200M</div>
+            <div class="el-upload__tip" slot="tip">只能上传rar/zip文件，且不超过300M</div>
         </el-upload>
         <span slot="footer" class="dialog-footer">
             <el-button type="primary" @click="handleOk" v-if="fileList.length > 0" class="dialogButtonW">确认</el-button>
@@ -64,7 +64,7 @@ export default {
         },
         beforeAvatarUpload(file) {
             let isZip = file.type.indexOf('zip') !== -1 || file.type.indexOf('rar') !== -1
-            const isLtM = file.size / 1024 / 1024 < 200
+            const isLtM = file.size / 1024 / 1024 < 300
             if (file.type == '' && file.name) {
                 let arrayTemp = file.name.split('.')
                 let fileType = arrayTemp[1]
@@ -74,7 +74,7 @@ export default {
                 this.$message.error('上传文件类型只能是 rar/zip 格式!')
             }
             if (!isLtM) {
-                this.$message.error('上传文件大小不能超过 200MB!')
+                this.$message.error('上传文件大小不能超过 300MB!')
             }
             if (!isZip || !isLtM) {
                 this.upLoadStatus = '0'
